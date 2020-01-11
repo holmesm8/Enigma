f2e8e6598220940c9f4392cf7e1f7fcbd34d9fc8
Matt Holmes <holmesm8@gmail.com>
2020-01-11 14:02:22 -0700

    Create method a_return from the key

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,3 +1,23 @@
+require './lib/key_gen'
+require './lib/date_gen'
+
 class Shift
 
+  def a_return(key)
+    key[0..1].to_i
+  end
+
+  def b_return(key)
+    key[1..2].to_i
+  end
+
+  def c_return(key)
+    key[2..3].to_i
+  end
+
+  def d_return(key)
+    key[3..4].to_i
+  end
+
+  
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -1,13 +1,31 @@
 require_relative './test_helper'
-require './lib/key'
+require './lib/key_gen'
+require './lib/date_gen'
+require './lib/shift'
 
 class ShiftTest < Minitest::Test
 
   def setup
     @shift1 = Shift.new
+    @key1 = "02715"
+    @date1 = "040895"
+    # @key1 = KeyGen.generate_random_key
+    # @date1 = DateGen.current_date
   end
 
   def test_it_exists
     assert_instance_of Shift, @shift1
   end
+
+  def test_it_has_attributes
+    skip
+  end
+
+  def test_it_can_return_key_placements
+    assert_equal 02, @shift1.a_return(@key1)
+    assert_equal 27, @shift1.b_return(@key1)
+    assert_equal 71, @shift1.c_return(@key1)
+    assert_equal 15, @shift1.d_return(@key1)
+  end
+
 end
