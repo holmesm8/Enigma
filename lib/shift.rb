d46605f8eba85d874e10ed1a5dec809338505494
Matt Holmes <holmesm8@gmail.com>
2020-01-11 14:49:15 -0700

    Update methods to class methods. Update tests as well

--- lib/shift.rb
+++ lib/shift.rb
@@ -3,28 +3,27 @@ require './lib/date_gen'
 
 class Shift
 
-  @offset = 0
-
-  def a_return(key)
+  def self.a_return(key)
     key[0..1].to_i
   end
 
-  def b_return(key)
+  def self.b_return(key)
     key[1..2].to_i
   end
 
-  def c_return(key)
+  def self.c_return(key)
     key[2..3].to_i
   end
 
-  def d_return(key)
+  def self.d_return(key)
     key[3..4].to_i
   end
 
-  def create_offset(date)
+  def self.create_offset(date)
     date_squared = date.to_i ** 2
-    @offset = date_squared.to_s[-4..-1].to_i
+    date_squared.to_s[-4..-1].to_i
   end
+
 
 
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -6,15 +6,11 @@ require './lib/shift'
 class ShiftTest < Minitest::Test
 
   def setup
-    @shift1 = Shift.new
+    # @shift1 = Shift.new
     @key1 = "02715"
     @date1 = "040895"
     # @key1 = KeyGen.generate_random_key
     # @date1 = DateGen.current_date
-  end
-
-  def test_it_exists
-    assert_instance_of Shift, @shift1
   end
 
   def test_it_has_attributes
@@ -22,23 +18,23 @@ class ShiftTest < Minitest::Test
   end
 
   def test_it_can_return_key_placements
-    assert_equal 02, @shift1.a_return(@key1)
-    assert_equal 27, @shift1.b_return(@key1)
-    assert_equal 71, @shift1.c_return(@key1)
-    assert_equal 15, @shift1.d_return(@key1)
+    assert_equal 02, Shift.a_return(@key1)
+    assert_equal 27, Shift.b_return(@key1)
+    assert_equal 71, Shift.c_return(@key1)
+    assert_equal 15, Shift.d_return(@key1)
   end
 
   def test_it_can_create_offset
-    assert_equal 1025, @shift1.create_offset(@date1)
+    assert_equal 1025, Shift.create_offset(@date1)
 
   end
 
 
   def test_it_can_create_shift
     skip
-    assert_equal 3, @shift1.a_shift(@date1)
-    assert_equal 27, @shift1.b_shift(@date1)
-    assert_equal 73, @shift1.c_shift(@date1)
-    assert_equal 20, @shift1.d_shift(@date1)
+    assert_equal 3, Shift.a_shift(@date1)
+    assert_equal 27, Shift.b_shift(@date1)
+    assert_equal 73, Shift.c_shift(@date1)
+    assert_equal 20, Shift.d_shift(@date1)
   end
 end
