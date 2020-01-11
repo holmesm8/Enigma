c0896dc62549bd80a3d2b65ba293ff264250d9bf
Matt Holmes <holmesm8@gmail.com>
2020-01-11 10:12:50 -0700

    Finish setup

---
+++ test/enigma_test.rb
@@ -0,0 +1,31 @@
+require_relative './test_helper'
+require './lib/key'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_can_encrypt
+    skip
+    expected = {encryption: "keder ohulw",
+                key: "02715",
+                date: "040895"
+                }
+    assert_equal expected, @enigma.encrypt("Hello world", "02715", "040895")
+  end
+
+  def test_it_can_decrypt
+    skip
+    expected = {decryption: "hello world",
+                key: "02715",
+                date: "040895"
+                }
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+end
--- test/key_test.rb
+++ test/key_test.rb
@@ -1,1 +1,15 @@
 require_relative './test_helper'
+require './lib/key'
+
+class KeyTest < Minitest::Test
+
+  def setup
+    @key = Key.new
+  end
+
+  def test_it_exists
+    assert_instance_of Key, @key
+  end
+
+
+end
