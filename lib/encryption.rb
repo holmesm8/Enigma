0705881beabeb2cb96e5f210b33a7ea6eb114181
Matt Holmes <holmesm8@gmail.com>
2020-01-12 18:13:41 -0700

    Refactor encryption and shift class. Switching from hash to array for shift values

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,24 +1,46 @@
 require './lib/shift'
 
 class Encryption
-  attr_reader :letters_array
+  attr_reader :alphabet
 
   def initialize
-    @letters_array = ("a".."z").to_a << " "
-  end
-
-  def message_array(message)
-    mess_array = message.split("")
+    @alphabet = ("a".."z").to_a << " "
   end
 
 
+  def shift_message(message, key, date)
+
+  end
+
+  # def alphabet_index_pairs
+  #   @alphabet.map.with_index(0).to_a
+  # end
+  #
+  # def shift_values(key, date)
+  #   Shift.shift_hash(key, date)
+  # end
+  #
+  # def shifted_alphabet
+  #   testy2 = alphabet_index_pairs.map do |letter, index|
+  #     require "pry"; binding.pry
+  #      testy = shift_values[index % 4]
+  #      (testy + number) % 27
+  #   end
+  #   require "pry"; binding.pry
+  # end
+  #
+  # def message_array(message, key, date)
+  #   # message.split("")
+  # end
+  #
+  # def message_shift(message, key, date)
+  #   shift = Shift.shift_array(key, date)
+  #   message.downcase.chars.map do |test|
+  #   end
+  # end
+
+end
   # def shift_thing(message, key, date)
   #   shifty_hash = Shift.shift_hash(key, date)
   #   message_array = message.split("")
-  #   require "pry"; binding.pry
-    # return shifty_hash[:a] if index % 4 == 0
-    # return shifty_hash[:b] if index % 4 == 1
-    # return shifty_hash[:c] if index % 4 == 2
-    # return shifty_hash[:d] if index % 4 == 3
   # end
-end
--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -17,15 +17,31 @@ class EncryptionTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "], @encryption1.letters_array
+    assert_equal ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "], @encryption1.alphabet
   end
 
-  def test_it_can_turn_message_string_into_array
-    assert_equal ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"], @encryption1.message_array(@message1)
+  def test_it_can_create_alphabet_index_pairs
+    skip
+    assert_equal [["a", 0], ["b", 1], ["c", 2], ["d", 3], ["e", 4], ["f", 5], ["g", 6], ["h", 7], ["i", 8], ["j", 9], ["k", 10], ["l", 11], ["m", 12], ["n", 13], ["o", 14], ["p", 15],
+ ["q", 16], ["r", 17], ["s", 18], ["t", 19], ["u", 20], ["v", 21], ["w", 22], ["x", 23], ["y", 24], ["z", 25], [" ", 26]], @encryption1.alphabet_index_pairs
   end
 
-  # def test_it_can_change_message1_with_shift_hash
-  #   assert_equal "keder ohulw!", @encryption1.shift_thing(@message1, @key1, @date1)
+  def test_it_can_return_shift_hash
+    skip
+    assert_equal ({a: 3, b: 27, c: 73, d: 20}), @encryption1.shift_values(@key1, @date1)
+  end
+
+  # def test_it_can_shift_alphabet
+  #   skip
+  #   assert_equal 1, @encryption1.shifted_alphabet
   # end
 
+  # def test_it_can_turn_message_string_into_array
+  #   skip
+  #   assert_equal ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"], @encryption1.message_array(@message1, @key1, @date1)
+  # end
+
+  def test_it_can_change_message_with_shift_hash
+    assert_equal "keder ohulw!", @encryption1.shifter(@message1, @key1, @date1)
+  end
 end
